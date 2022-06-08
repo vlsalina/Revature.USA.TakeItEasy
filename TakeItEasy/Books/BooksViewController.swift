5830241b5efc2ee019c9ec4e52e9dabc218e1b36
Curtis Becker <ceb2941@gmail.com>
2022-06-07 22:36:35 -0400

    added function for setting up corner radius

--- TakeItEasy/Books/BooksViewController.swift
+++ TakeItEasy/Books/BooksViewController.swift
@@ -32,48 +32,42 @@ class BooksViewController: UIViewController, UICollectionViewDelegate, UICollect
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         switch collectionView.restorationIdentifier{
         case "GeneralCollectionView":
-            var generalCell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralCell", for: indexPath) as! GeneralBooksCollectionViewCell
-            generalCell = setupGeneralCell(generalCell, indexPath)
-            return generalCell
-            
+            let generalCell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralCell", for: indexPath) as! GeneralBooksCollectionViewCell
+            return setupGeneralCell(generalCell, indexPath)
         case "TechnologyCollectionView":
-            var technologyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TechnologyCell", for: indexPath) as! TechnologyBooksCollectionViewCell
-            technologyCell = setupTechnologyCell(technologyCell, indexPath)
-            return technologyCell
-            
+            let technologyCell = collectionView.dequeueReusableCell(withReuseIdentifier: "TechnologyCell", for: indexPath) as! TechnologyBooksCollectionViewCell
+            return setupTechnologyCell(technologyCell, indexPath)
         case "RecipesCollectionView":
-            var recipeCell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCell", for: indexPath) as! RecipeBooksCollectionViewCell
-            recipeCell = setupRecipeCell(recipeCell, indexPath)
-            return recipeCell
-            
+            let recipeCell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCell", for: indexPath) as! RecipeBooksCollectionViewCell
+            return setupRecipeCell(recipeCell, indexPath)
         default:
             let generalCell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralCell", for: indexPath) as! GeneralBooksCollectionViewCell
             return generalCell
         }
     }
     
-    func setupGeneralCell(_ generalCell : GeneralBooksCollectionViewCell, _ indexPath : IndexPath) -> GeneralBooksCollectionViewCell{
+    func setupGeneralCell(_ generalCell : GeneralBooksCollectionViewCell, _ indexPath : IndexPath) -> UICollectionViewCell{
         generalCell.bookLabel.text = generalText[indexPath.row]
         generalCell.backgroundColor = .systemGray
-        generalCell.layer.cornerRadius = 10
-        generalCell.layer.masksToBounds = true
-        return generalCell
+        return setCornerRadiuForCell(generalCell)
     }
 
-    func setupTechnologyCell(_ technologyCell : TechnologyBooksCollectionViewCell, _ indexPath : IndexPath) -> TechnologyBooksCollectionViewCell{
+    func setupTechnologyCell(_ technologyCell : TechnologyBooksCollectionViewCell, _ indexPath : IndexPath) -> UICollectionViewCell{
         technologyCell.bookLabel.text = technologyText[indexPath.row]
         technologyCell.backgroundColor = .systemGray2
-        technologyCell.layer.cornerRadius = 10
-        technologyCell.layer.masksToBounds = true
-        return technologyCell
+        return setCornerRadiuForCell(technologyCell)
     }
     
-    func setupRecipeCell(_ recipeCell : RecipeBooksCollectionViewCell, _ indexPath : IndexPath) -> RecipeBooksCollectionViewCell{
+    func setupRecipeCell(_ recipeCell : RecipeBooksCollectionViewCell, _ indexPath : IndexPath) -> UICollectionViewCell{
         recipeCell.bookLabel.text = recipeText[indexPath.row]
         recipeCell.backgroundColor = .systemGray4
-        recipeCell.layer.cornerRadius = 10
-        recipeCell.layer.masksToBounds = true
-        return recipeCell
+        return setCornerRadiuForCell(recipeCell)
+    }
+    
+    func setCornerRadiuForCell(_ collectionViewCell : UICollectionViewCell) -> UICollectionViewCell{
+        collectionViewCell.layer.cornerRadius = 10
+        collectionViewCell.layer.masksToBounds = true
+        return collectionViewCell
     }
     
 }

45d0e5d6759ffb7fbf8f95c336fa07d9a9b19a13
Curtis Becker <ceb2941@gmail.com>
2022-06-07 23:28:56 -0400

    added navigation bar data singleton class for displaying username data on navigation bar across classes

---
+++ TakeItEasy/NavigationBarData.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarData.swift
+//  TakeItEasy
+//
+//  Created by admin on 6/7/22.
+//
+
+import Foundation
+
+class NavigationBarData{
+    static var DataObject = NavigationBarData()
+    var username = "test"
+
+    private init(){
+        
+    }
+}

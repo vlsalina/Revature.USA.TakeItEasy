98eb4c14ee5707ad3bcaf81b4ad1e95475b5facc
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-09 13:51:57 -0700

    Finished basic layout for Questions page.

--- TakeItEasy/Controllers/QuestionsPage/QuestionsPageViewController.swift
+++ TakeItEasy/Controllers/QuestionsPage/QuestionsPageViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class QuestionsPageViewController: UIViewController {
-
+    
+    @IBOutlet weak var questoinCollection: UICollectionView!
+    var quiz : Quiz?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension QuestionsPageViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (quiz?.questions.count)!
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    }
+    
+    
+    
 }

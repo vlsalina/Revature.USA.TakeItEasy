962cd66e171d71df8b599783adffa18b7dd84722
MatthewCaulder <matt.caulder@gmail.com>
2022-06-09 09:38:45 -0400

    Basic Music Layout

---
+++ TakeItEasy/Music/MusicViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MusicViewController.swift
+//  TakeItEasy
+//
+//  Created by Matt Caulder on 6/9/22.
+//
+
+import UIKit
+import AVFoundation
+class MusicViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "musicCell", for: indexPath)
+        
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

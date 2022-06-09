924e251655c0e72e0b78a315c2c222b80a51151c
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-08 18:36:56 -0700

    Implemented login functionality, error handling, and remember me functionality for login page.

--- TakeItEasy/Helpers/Helpers.swift
+++ TakeItEasy/Helpers/Helpers.swift
@@ -10,4 +10,18 @@ import UIKit
 
 let userDefaults = UserDefaults.standard
 
+// validate login credentials
+internal func validateLoginCredentials(userid: String, password: String) throws {
+    
+    guard (!userid.isEmpty) else {
+        throw LoginErrors.invalidLoginCredentials
+    }
+    
+    guard (!password.isEmpty) else {
+        throw LoginErrors.invalidLoginCredentials
+    }
+    
+}
+
+
 

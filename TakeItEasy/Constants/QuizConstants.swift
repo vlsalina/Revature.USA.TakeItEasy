b6ce7142312aebf6553afc387bd3c02e04ee0113
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-11 11:30:31 -0700

    Created Quiz data for SQLite database.

--- TakeItEasy/Constants/QuizConstants.swift
+++ TakeItEasy/Constants/QuizConstants.swift
@@ -14,16 +14,24 @@ enum QuizKeys : Int {
     case QUIZ4 = 3
 }
 
-enum QuizChoices : Int {
-    case A = 0
-    case B = 1
-    case C = 2
-    case D = 3
-}
-
-enum QuizErrors : Error {
-    case notAllQuestionsAnswered
-}
+//enum QuizQuestionKeys : Int {
+//    case Q1 = 0
+//    case Q2 = 1
+//    case Q3 = 2
+//    case Q4 = 3
+//    case Q5 = 4
+//}
+//
+//enum QuizChoices : Int {
+//    case A = 0
+//    case B = 1
+//    case C = 2
+//    case D = 3
+//}
+//
+//enum QuizErrors : Error {
+//    case notAllQuestionsAnswered
+//}
 
 enum QuizConstants : String {
     case notAllQuestionsAnswered = "Please answer all questions"
--- TakeItEasy/DataFolder/QuizData.swift
+++ TakeItEasy/DataFolder/QuizData.swift
@@ -9,8 +9,60 @@ import Foundation
 import UIKit
 
 let quizzes = [
-    Quiz2(name: "SWIFT", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, key: QuizKeys.QUIZ1.rawValue),
-    Quiz2(name: "XCODE", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, key: QuizKeys.QUIZ1.rawValue),
-    Quiz2(name: "STORYBOARD", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, key: QuizKeys.QUIZ1.rawValue),
-    Quiz2(name: "CLASSES", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, key: QuizKeys.QUIZ1.rawValue),
+    QuizSQLClass(name: "SWIFT", image: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview", qkey: QuizKeys.QUIZ1.rawValue),
+    QuizSQLClass(name: "XCODE", image: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview", qkey: QuizKeys.QUIZ2.rawValue),
+    QuizSQLClass(name: "STORYBOARD", image: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview", qkey: QuizKeys.QUIZ3.rawValue),
+    QuizSQLClass(name: "COLLECTIONS", image: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview", qkey: QuizKeys.QUIZ4.rawValue),
 ]
+
+//let questions = [
+//
+//    // Quiz 1 questions
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q2.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q3.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q4.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q5.rawValue),
+//
+//
+//    // Quiz 2 questions
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ2.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ2.rawValue, qqkey: QuizQuestionKeys.Q2.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ2.rawValue, qqkey: QuizQuestionKeys.Q3.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ2.rawValue, qqkey: QuizQuestionKeys.Q4.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ2.rawValue, qqkey: QuizQuestionKeys.Q5.rawValue),
+//
+//
+//
+//
+//    // Quiz 3 questions
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ3.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ3.rawValue, qqkey: QuizQuestionKeys.Q2.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ3.rawValue, qqkey: QuizQuestionKeys.Q3.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ3.rawValue, qqkey: QuizQuestionKeys.Q4.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ3.rawValue, qqkey: QuizQuestionKeys.Q5.rawValue),
+//
+//
+//
+//
+//    // Quiz 4 questions
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ4.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ4.rawValue, qqkey: QuizQuestionKeys.Q2.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ4.rawValue, qqkey: QuizQuestionKeys.Q3.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ4.rawValue, qqkey: QuizQuestionKeys.Q4.rawValue),
+//    Question2(question: "QUESTION", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, correctAnswer: QuizChoices.A.rawValue, qkey: QuizKeys.QUIZ4.rawValue, qqkey: QuizQuestionKeys.Q5.rawValue),
+//]
+//
+//
+//
+//let choices = [
+//    // For Quiz1, Quesiton1
+//    Choice2(choice: "CHOICE", qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue, ckey: QuizChoices.A.rawValue)
+//    Choice2(choice: "CHOICE", qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue, ckey: QuizChoices.A.rawValue)
+//    Choice2(choice: "CHOICE", qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue, ckey: QuizChoices.A.rawValue)
+//    Choice2(choice: "CHOICE", qkey: QuizKeys.QUIZ1.rawValue, qqkey: QuizQuestionKeys.Q1.rawValue, ckey: QuizChoices.A.rawValue)
+//]
+
+
+
+

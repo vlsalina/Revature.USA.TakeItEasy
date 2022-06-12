abf91aba2f17222b3c07c1d21a8d976e2bbb38d4
Vincent Salinas <vincent.salinas.2022@gmail.com>
2022-06-11 23:39:42 -0700

    Included questions and answers for Swift quiz.

--- TakeItEasy/Classes/Quiz.swift
+++ TakeItEasy/Classes/Quiz.swift
@@ -25,36 +25,36 @@ class Quiz {
         return [
             // Quiz
             Quiz(name: "Swift", image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!, details: Details(questions: [
-                Question(question: "a;slfdkjas;lkfdja;sldkfjas;ldfkja;sdlfkja;sdlfkja;sdlkfja;sldfkj", choices: [
-                    Choice(choice: "a"),
-                    Choice(choice: "b"),
-                    Choice(choice: "c"),
-                    Choice(choice: "d"),
-                ], correctAnswer: Choice(choice: "a"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
-                Question(question: "a;slfdkjas;lkfdja;sldkfjas;ldfkja;sdlfkja;sdlfkja;sdlkfja;sldfkj", choices: [
-                    Choice(choice: "a"),
-                    Choice(choice: "b"),
-                    Choice(choice: "c"),
-                    Choice(choice: "d"),
-                ], correctAnswer: Choice(choice: "a"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
-                Question(question: "a;slfdkjas;lkfdja;sldkfjas;ldfkja;sdlfkja;sdlfkja;sdlkfja;sldfkj", choices: [
-                    Choice(choice: "a"),
-                    Choice(choice: "b"),
-                    Choice(choice: "c"),
-                    Choice(choice: "d"),
-                ], correctAnswer: Choice(choice: "a"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
-                Question(question: "a;slfdkjas;lkfdja;sldkfjas;ldfkja;sdlfkja;sdlfkja;sdlkfja;sldfkj", choices: [
-                    Choice(choice: "a"),
-                    Choice(choice: "b"),
-                    Choice(choice: "c"),
-                    Choice(choice: "d"),
-                ], correctAnswer: Choice(choice: "a"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
-                Question(question: "a;slfdkjas;lkfdja;sldkfjas;ldfkja;sdlfkja;sdlfkja;sdlkfja;sldfkj", choices: [
-                    Choice(choice: "a"),
-                    Choice(choice: "b"),
-                    Choice(choice: "c"),
-                    Choice(choice: "d"),
-                ], correctAnswer: Choice(choice: "a"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
+                Question(question: "iOS Swift is a programming language developed by...", choices: [
+                    Choice(choice: "Microsoft"),
+                    Choice(choice: "Google"),
+                    Choice(choice: "Apple"),
+                    Choice(choice: "Amazon"),
+                ], correctAnswer: Choice(choice: "Apple"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
+                Question(question: "What is init() in Swift?", choices: [
+                    Choice(choice: "an initializer"),
+                    Choice(choice: "a protocol"),
+                    Choice(choice: "a class"),
+                    Choice(choice: "an enum"),
+                ], correctAnswer: Choice(choice: "an initializer"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
+                Question(question: "\"Let\" variables are...", choices: [
+                    Choice(choice: "nil"),
+                    Choice(choice: "changeable"),
+                    Choice(choice: "formable"),
+                    Choice(choice: "constant"),
+                ], correctAnswer: Choice(choice: "constant"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
+                Question(question: "Inheritance is a mechanism for ____?", choices: [
+                    Choice(choice: "functions"),
+                    Choice(choice: "generics"),
+                    Choice(choice: "Object Oriented Programming"),
+                    Choice(choice: "building in storyboard"),
+                ], correctAnswer: Choice(choice: "Object Oriented Programming"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
+                Question(question: "Objects have state and ____", choices: [
+                    Choice(choice: "behaviors"),
+                    Choice(choice: "encapsulation"),
+                    Choice(choice: "abstraction"),
+                    Choice(choice: "polymorphism"),
+                ], correctAnswer: Choice(choice: "behaviors"), image: UIImage(named: "harry_potter_wallpaper_digital_design____11_by_sefabaris713_de5yl8r-fullview")!),
             ])
             ),
             
